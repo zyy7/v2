901753abf779eccc1053712cc93e62cb91983b3b
zyy7 <101653647+zyy7@users.noreply.github.com>
2022-03-26 12:29:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285924761802', '6285924761802','6285943140485'] // Letakan nomor kamu disini
+global.owner = ['6285924761802', '6285943140485','6285924761802', '6285943140485', '6285924761802'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
